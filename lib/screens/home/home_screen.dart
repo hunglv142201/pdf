e537489eae6fb9df1f0fa926fc2a2382e439e8ac
Nguyen Nhat Quang <84659729+nhatquang237@users.noreply.github.com>
2022-09-28 00:15:14 +0700

    Form 12_1_27

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:pdf_creator/pdf_templates/test_2.dart';
 import 'package:pdf_creator/pdf_templates/test_3.dart';
 import 'package:pdf_creator/pdf_templates/test_4.dart';
 import 'package:pdf_creator/pdf_templates/test_10_1_25.dart';
+import 'package:pdf_creator/pdf_templates/test_12_1_27.dart';
 import 'package:pdf_creator/screens/pdf_viewer/pdf_viewer_screen.props.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -24,6 +25,8 @@ class HomeScreen extends StatelessWidget {
               _buildLink(Test4PdfTemplate(), "To PDF 3_1_4 Template", context),
               _buildLink(
                   Test10125PdfTemplate(), "To PDF 10_1_25 Template", context),
+              _buildLink(Test12PdfTemplate('2022', '07', '01', '株式会社ＡＮＡＬＯＧ'),
+                  "To PDF 12_1_27 Template", context),
             ],
           ),
         ),
