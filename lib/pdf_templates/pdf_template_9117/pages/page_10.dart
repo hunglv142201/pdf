43f948e959100f7e14fdcce1eeac988424de52b9
Nguyen Nhat Quang <84659729+nhatquang237@users.noreply.github.com>
2022-11-01 00:27:34 +0700

    [IMP] pdf_template_9117 page 10 Save progress page 9

---
+++ lib/pdf_templates/pdf_template_9117/pages/page_10.dart
@@ -0,0 +1,132 @@
+import 'package:pdf/widgets.dart';
+import 'package:pdf_creator/pdf_templates/pdf_template_9117/pages/common.dart';
+
+import 'package:pdf/pdf.dart';
+
+class Page10 {
+  static Widget buildPage(List<String> inputs, Font font) {
+    List<String> ins = [
+      '株式会社ＡＮＡＬＯＧ', // 1
+      '社長', // 2
+      '作成ゆみこ', // 3
+      '日本語日本語', // 4
+      '2022/10/31', // 5
+    ];
+
+    String year = ins[4] == '' ? '' : ins[4].split('/')[0];
+    String month = ins[4] == '' ? '' : ins[4].split('/')[1];
+    String day = ins[4] == '' ? '' : ins[4].split('/')[2];
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildText('（注意）', font, fontSize: 11),
+        _buildText(
+            '１ Ⅰ欄は，支援対象者が複数名いる場合であって支援内容が同一の場合に限り，「１ 氏名に「別紙の名簿のとおり」と記載し，Ⅰ欄の事項が記載された名簿（任意様式）を添付することとして差し支えない。ただし， 申請を同時に行う場合に限る。',
+            font,
+            fontSize: 11),
+        _buildText('２ Ⅱ欄４は，特定技能所属機関自らが１号特定技能外国人支援計画を実施する場合のみ記載すること', font, fontSize: 11),
+        _buildText('３ Ⅲ欄は，１号特定技能外国人支援計画の全部の実施を登録支援機関に委託する場合のみ記載すること。', font, fontSize: 11),
+        _buildText('４ Ⅳ欄１から９までの「実施予定」欄は，該当するものにチェックをし，実施予定が「有」の場合は実施時期を簡潔に記載し，「無」の場合は当該支援を実施しない理由について括弧内に記載すること', font,
+            fontSize: 11),
+        _buildText(
+            '５ Ⅳ欄１から９までの「委託の有無」欄は，特定技能所属機関が支援の一部を第三者に委託（登録支援機関に委託する場合を除く。）する場合にのみ「有」と記載し，特定技能所属機関が自ら支援を行う場合や  登録支援機関に支援の全部を委託する場合には「無」と記載すること。',
+            font,
+            fontSize: 11),
+        _buildText(
+            '６ Ⅳ欄１から８までの「支援担当者又は委託を受けた支援実施者」欄は，特定技能所属機関が自ら支援を行う場合や登録支援機関に 支援の全部を委託する場合に選任することとされている支援担当者 を 支援項目ごとに氏名及び括弧内に役職を記載し，特定技能所属機関から委託の一部を受けた第三者が支援を実施する場合は支援の委託を受けた者の氏名及び所在地を記載すること。',
+            font,
+            fontSize: 11),
+        _buildText(
+            '   なお，Ⅳ欄９の「支援責任者又は支援担当者」欄は，特定技能所属機関が自ら支援を行う場合や登録支援機関に支援の全部を委託する場合に選任することとされている支援責任者又は支援担当者の氏名及び括弧内に役職を記載し,このうちｂ欄及び自由記入欄については同欄の支援を,特定技能所属機関から委託の一部を受けた第三者が支援を実施する場合は支援の委託を受けた者の氏名及び所在地を記載すること。',
+            font,
+            fontSize: 11),
+        _buildText('７ Ⅳ欄１から４まで及び９の「実施方法」欄は，該当するものにチェックをすること。「その他」にチェックする場合は，その実施方法について括弧内に記載すること。', font, fontSize: 11),
+        _buildText('８ Ⅳ欄３アｄ「居室の広さ」について，該当するものを括弧内のチェックボックスから選択すること。', font, fontSize: 11),
+        _buildText('９ Ⅳ欄６イの「対応時間」欄は，対応可能曜日についてその時間帯を記載すること。', font, fontSize: 11),
+        Padding(padding: const EdgeInsets.only(top: 20), child: _buildText('上記のとおり支援を実施します。', font, fontSize: 13)),
+        Padding(
+            padding: const EdgeInsets.only(bottom: 40),
+            child: _buildText('また，本書面は支援対象者が十分に理解できる言語に翻訳し，支援対象者に交付するとともに，内容について十分に説明しました。', font, fontSize: 13)),
+        Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+          Container(
+            alignment: Alignment.centerRight,
+            width: 300,
+            padding: const EdgeInsets.fromLTRB(10, 10, 20, 10),
+            child: _buildText('特定技能所属機関の氏名又は名称', font, fontSize: 13),
+          ),
+          Container(
+              alignment: Alignment.center,
+              width: 300,
+              decoration: _buildBoxUnderLine(width: 1),
+              child: _buildText(ins[0], font, fontSize: 13))
+        ]),
+        Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+          Container(
+            alignment: Alignment.centerRight,
+            width: 300,
+            padding: const EdgeInsets.fromLTRB(10, 10, 20, 10),
+            child: _buildText('作    成    責    任    者    の    氏    名', font, fontSize: 13),
+          ),
+          Container(
+              alignment: Alignment.center,
+              width: 300,
+              decoration: _buildBoxUnderLine(width: 1),
+              child: _buildText(ins[1] + '    ' + ins[2], font, fontSize: 13)),
+        ]),
+        Padding(
+          padding: const EdgeInsets.only(top: 10, bottom: 10),
+          child: Row(children: [
+            _buildText('本書面について，', font, fontSize: 13),
+            Container(
+              width: 150,
+              alignment: Alignment.center,
+              decoration: _buildBoxUnderLine(width: 1),
+              child: _buildText(ins[3], font, fontSize: 13),
+            ),
+            _buildText(' 語による翻訳文の交付を受け，その内容について説明を受け十分に理解しました。', font, fontSize: 13),
+          ]),
+        ),
+        Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+          Container(
+            alignment: Alignment.centerRight,
+            width: 300,
+            padding: const EdgeInsets.fromLTRB(10, 10, 20, 10),
+            child: _buildText('署                   名                   日', font, fontSize: 13),
+          ),
+          Container(
+              alignment: Alignment.center,
+              width: 300,
+              child: _buildText(year + ' 年  ' + month + ' 月 ' + day + ' 日 ', font, fontSize: 13))
+        ]),
+        Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+          Container(
+            alignment: Alignment.centerRight,
+            width: 300,
+            padding: const EdgeInsets.fromLTRB(10, 10, 20, 10),
+            child: _buildText('１  号  特  定  技  能  外  国  人  の  署  名', font, fontSize: 13),
+          ),
+          Container(
+              alignment: Alignment.center,
+              width: 300,
+              decoration: _buildBoxUnderLine(width: 1),
+              child: _buildText(ins[1] + '    ' + ins[2], font, fontSize: 13)),
+        ]),
+      ],
+    );
+  }
+
+  static Widget _buildText(String text, Font font, {double fontSize = 0}) {
+    if (fontSize == 0) {
+      return Text(text, style: TextStyle(font: font));
+    } else {
+      return Text(text, style: TextStyle(font: font, fontSize: fontSize));
+    }
+  }
+
+  static BoxDecoration _buildBoxUnderLine({String color = '#000000', double width = 1}) {
+    return BoxDecoration(
+      border: Border(bottom: BorderSide(color: PdfColor.fromHex(color), width: width)),
+    );
+  }
+}
---
+++ lib/pdf_templates/pdf_template_9117/pages/page_9.dart
@@ -0,0 +1,155 @@
+import 'package:pdf/widgets.dart';
+import 'package:pdf_creator/pdf_templates/pdf_template_9117/pages/common.dart';
+
+class Page9 {
+  static Widget buildPage(List<String> inputs, Font font) {
+    List<String> ins = [
+      '1Hello World!', // 0
+      '2', // 1
+      'asd asd asd', // 2
+      'Testing here', // 3
+      '', // 4
+      '1Hello World!', // 5
+      '2', // 6
+      'asd asd asd', // 7
+      'Testing here', // 8
+      '', // 9
+      'Hi there', // 10
+      '', // 11
+      '', // 12
+      '', // 13
+      '', // 14
+      '', // 15
+      '', // 16
+      '', // 17
+      '', // 18
+      '', // 19
+      '', // 20
+      '', // 21
+      '', // 22
+      '', // 23
+      '', // 24
+      '', // 25
+      '', // 26
+      '', // 27
+      '', // 28
+      '', // 29
+      '', // 30
+      '', // 31
+      '', // 32
+      '', // 33
+      '', // 34
+      '', // 35
+      '', // 36
+      '', // 37
+      '', // 38
+      '', // 39
+      '', // 40
+      '', // 41
+      '', // 42
+    ];
+
+    Common c = Common(font);
+
+    return Row(
+      children: [
+        c.box(c.verticalText('Ⅳ支援内容（続き）')),
+        Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                c.box(c.verticalText('職支援（続き）\n８ 非自発的離職時の転')),
+                Table(
+                  defaultVerticalAlignment: TableCellVerticalAlignment.full,
+                  children: [
+                    // --- 2.1 ---
+                    TableRow(children: [
+                      c.boxText(('f．離職時に必要な行政手続について情報を提供する'), width: 195, height: 50),
+                      c.box(c.booleanInputWithText(ins[0]), width: 160),
+                      c.box(c.optionInput(ins[1]), width: 70),
+                      Row(children: [
+                        c.boxText(ins[2], width: 115, height: 50),
+                        c.boxText(ins[3], width: 260, height: 50),
+                      ]),
+                      c.box(c.boxNoBorder(), width: 195),
+                    ]),
+
+                    // --- 2.2 ---
+                    TableRow(children: [
+                      c.boxText(('g．倒産等により，転職のための支援 が適切に実施できなくなることが見込 まれるときは，それに備え，当該機関 に代わって支援を行う者を確保する'),
+                          width: 195, height: 60),
+                      c.box(c.booleanInputWithText(ins[5]), width: 160),
+                      c.box(c.optionInput(ins[6]), width: 70),
+                      Row(children: [
+                        c.boxText(ins[7], width: 115, height: 60),
+                        c.boxText(ins[8], width: 260, height: 60),
+                      ]),
+                      c.box(c.boxNoBorder(), width: 195),
+                    ]),
+
+                    // --- 2.3 ---
+                    TableRow(children: [
+                      c.verticalBoxText(['（自由記入）', ins[10]], width: 195),
+                      c.box(c.booleanInputWithText(ins[11]), width: 160),
+                      c.box(c.optionInput(ins[12]), width: 70),
+                      Row(children: [
+                        c.boxText(ins[13], width: 115, height: 50),
+                        c.boxText(ins[14], width: 260, height: 50),
+                      ]),
+                      c.box(c.boxNoBorder(), width: 195),
+                    ]),
+                  ],
+                ),
+              ],
+            ),
+            Expanded(
+              child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                c.box(c.verticalText('９ 定期的な面談の実施・行政機関への通報')),
+                c.box(c.verticalText('ア   面談内容等')),
+                Table(defaultVerticalAlignment: TableCellVerticalAlignment.full, children: [
+                  // --- 3.1 ---
+                  TableRow(children: [
+                    c.boxText('支援内容', center: true, width: 177),
+                    c.boxText('実施予定', center: true, width: 160),
+                    c.verticalBoxText(['委託の', '有 無'], width: 70),
+                    Column(children: [
+                      c.boxText(('支援担当者又は委託を受けた実施担当者'), center: true, width: 375),
+                      Row(children: [
+                        c.verticalBoxText([('氏名'), ('（役職）')], width: 115),
+                        c.verticalBoxText([('住所'), ('（委託を受けた場合のみ）')], width: 260),
+                      ]),
+                    ]),
+                  ]),
+
+                  // --- 3.2 ---
+                  TableRow(children: [
+                    c.boxText(
+                        ('ａ．不動産仲介事業者や賃貸物件の情報を提供し，必要に応じて住宅確保に係る手続に同行し，住居探しの補助を行う。また，賃貸借契約の締結時に連帯保証人が必要な場合に，適当な連帯保証人がいないときは，支援対象者の連帯保証人となる又は利用可能な家賃債務保証業者を確保し自らが緊急連絡先となる'),
+                        width: 177),
+                    c.box(c.booleanInputWithText(ins[16]), width: 160),
+                    c.box(c.optionInput(ins[17]), width: 70),
+                    Row(children: [
+                      c.boxText(ins[18], width: 115, height: 150),
+                      c.boxText(ins[19], width: 260, height: 150),
+                    ]),
+                  ]),
+
+                  // --- 3.3 ---
+                  TableRow(children: []),
+
+                  // --- 3.4 ---
+                  TableRow(children: []),
+
+                  // --- 3.5 ---
+                  TableRow(children: []),
+                ]),
+                c.box(Container(), width: 195),
+              ]),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
