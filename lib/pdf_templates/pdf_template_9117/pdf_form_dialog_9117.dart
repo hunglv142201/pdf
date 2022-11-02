1a5516e79ba684d3d20e87fbefa3ec18c3d94e93
Nguyen Nhat Quang <84659729+nhatquang237@users.noreply.github.com>
2022-11-02 19:47:08 +0700

    Update form dialog and template

--- lib/pdf_templates/pdf_template_9117/pdf_form_dialog_9117.dart
+++ lib/pdf_templates/pdf_template_9117/pdf_form_dialog_9117.dart
@@ -12,26 +12,72 @@ class PdfFormDialog9117 extends StatelessWidget {
       TableRowProps('input1_0 氏 名', InputType.text),
       TableRowProps('input1_1 氏 名 (....)', InputType.text),
       TableRowProps('input1_2 Date of Birth', InputType.date),
-      TableRowProps('input1_3 Gender', InputType.option,
-          params: {'1': '男', '2': '女'}),
+      TableRowProps('input1_3 Gender', InputType.option, params: {'1': '男', '2': '女'}),
       TableRowProps('input1_4 Age', InputType.text),
       TableRowProps('input1_5 検 診 年 月 日', InputType.date),
       TableRowProps('input1_6 作 成 年 月 日', InputType.date)
+    ],
+    [
+      // Page 2
+    ],
+    [
+      // Page 3
+    ],
+    [
+      // Page 4
+    ],
+    [
+      // Page 5
+    ],
+    [
+      // Page 6
+    ],
+    [
+      // Page 7
+    ],
+    [
+      // Page 8
+      TableRowProps('input8_0', InputType.text),
+      TableRowProps('input8_1', InputType.text),
+      TableRowProps('input8_2', InputType.text),
+      TableRowProps('input8_3', InputType.text),
+      TableRowProps('input8_4', InputType.text),
+      TableRowProps('input8_5', InputType.text),
+      TableRowProps('input8_6', InputType.text),
+    ],
+    [
+      // Page 9
+      TableRowProps('input9_0', InputType.text),
+      TableRowProps('input9_1', InputType.text),
+      TableRowProps('input9_2', InputType.text),
+      TableRowProps('input9_3', InputType.text),
+      TableRowProps('input9_4', InputType.text),
+      TableRowProps('input9_5', InputType.text),
+      TableRowProps('input9_6', InputType.text),
+    ],
+    [
+      // Page 10
+      TableRowProps('input10_0', InputType.text), //
+      TableRowProps('input10_1', InputType.text), //
+      TableRowProps('input10_2', InputType.text), //
+      TableRowProps('input10_3', InputType.text), //
+      TableRowProps('input10_4', InputType.date), //
+      TableRowProps('input10_5', InputType.text), //
+      TableRowProps('input10_6', InputType.text), //
     ],
   ];
 
   @override
   Widget build(BuildContext context) {
     void _callback(List<List<String>> inputs) {
+      print(_tableProps);
       PdfTemplate9117 template = PdfTemplate9117(inputs);
-
       Navigator.of(context).pushNamed(
         'pdf_viewer',
         arguments: PdfViewerScreenProps(pdf: template.build()),
       );
     }
 
-    return FormDialog(
-        title: '参考様式第 1 - 17 号', tableProps: _tableProps, callback: _callback);
+    return FormDialog(title: '参考様式第 1 - 17 号', tableProps: _tableProps, callback: _callback);
   }
 }
--- lib/pdf_templates/pdf_template_9117/pdf_template_9117.dart
+++ lib/pdf_templates/pdf_template_9117/pdf_template_9117.dart
@@ -8,6 +8,9 @@ import 'package:pdf_creator/pdf_templates/pdf_template_9117/pages/page_1.dart';
 import 'package:pdf_creator/pdf_templates/pdf_template_9117/pages/page_2.dart';
 import 'package:pdf_creator/pdf_templates/pdf_template_9117/pages/page_3.dart';
 import 'package:pdf_creator/pdf_templates/pdf_template_9117/pages/page_5.dart';
+import 'package:pdf_creator/pdf_templates/pdf_template_9117/pages/page_8.dart';
+import 'package:pdf_creator/pdf_templates/pdf_template_9117/pages/page_9.dart';
+import 'package:pdf_creator/pdf_templates/pdf_template_9117/pages/page_10.dart';
 import 'package:printing/printing.dart';
 
 class PdfTemplate9117 extends PDFTemplate {
@@ -20,34 +23,26 @@ class PdfTemplate9117 extends PDFTemplate {
   @override
   Future<Uint8List> build() async {
     font = await PdfGoogleFonts.mPLUSRounded1cRegular();
-
     buildPage(Page1.buildPage([], font),
         pageFormat: const PdfPageFormat(42 * cm, 29.7 * cm,
-            marginTop: 2 * cm,
-            marginBottom: 2 * cm,
-            marginLeft: 2 * cm,
-            marginRight: 2 * cm));
+            marginTop: 2 * cm, marginBottom: 2 * cm, marginLeft: 2 * cm, marginRight: 2 * cm));
     buildPage(Page2.buildPage([], font),
         pageFormat: const PdfPageFormat(42 * cm, 29.7 * cm,
-            marginTop: 3 * cm,
-            marginBottom: 2 * cm,
-            marginLeft: 2 * cm,
-            marginRight: 2 * cm));
+            marginTop: 3 * cm, marginBottom: 2 * cm, marginLeft: 2 * cm, marginRight: 2 * cm));
     buildPage(Page3.buildPage([], font),
         pageFormat: const PdfPageFormat(42 * cm, 29.7 * cm,
-            marginTop: 3 * cm,
-            marginBottom: 2 * cm,
-            marginLeft: 2 * cm,
-            marginRight: 2 * cm));
-    buildPage(Page4.buildPage([], font),
-        pageFormat: PdfPageFormat.a3,
-        pageOrientation: PageOrientation.landscape);
-    // buildPage(Page4.buildPage([], font), pageFormat: PdfPageFormat.a3, pageOrientation: PageOrientation.landscape);
-    buildPage(Page5.buildPage([], font),
-        pageFormat: PdfPageFormat.a3,
-        pageOrientation: PageOrientation.landscape);
-
+            marginTop: 3 * cm, marginBottom: 2 * cm, marginLeft: 2 * cm, marginRight: 2 * cm));
+    buildPage(Page4.buildPage([], font), pageFormat: PdfPageFormat.a3, pageOrientation: PageOrientation.landscape);
+    buildPage(Page5.buildPage([], font), pageFormat: PdfPageFormat.a3, pageOrientation: PageOrientation.landscape);
+    buildPage(Page8.buildPage(inputs[7], font), pageFormat: _builtA3Format(), pageOrientation: PageOrientation.natural);
+    buildPage(Page9.buildPage(inputs[8], font), pageFormat: _builtA3Format(), pageOrientation: PageOrientation.natural);
+    buildPage(Page10.buildPage(inputs[0], font),
+        pageFormat: _builtA3Format(), pageOrientation: PageOrientation.natural);
     return await pdf.save();
+  }
+
+  PdfPageFormat _builtA3Format() {
+    return const PdfPageFormat(42.0 * cm, 29.7 * cm, marginAll: 2.0 * cm);
   }
 
   static const double inch = 72.0;
